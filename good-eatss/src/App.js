c9b611e11fa78dea37736e3244e6a3e096551cc3
Ariana Jackson <ariana.desiree@gmail.com>
2023-02-27 00:40:05 -0600

    trying to fetch popular

---
+++ good-eatss/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Pages from './pages/Pages';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+     <h1> Hello World! </h1>
+    
+   <Pages />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ good-eatss/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+
+import Chicken from "../components/Chicken";
+import Popular from "../components/Popular";
+import React from 'react'
+
+
+function Home() {
+  return (
+    <div>
+
+        <Chicken />
+        <Popular />
+
+    </div>
+  )
+}
+
+export default Home
